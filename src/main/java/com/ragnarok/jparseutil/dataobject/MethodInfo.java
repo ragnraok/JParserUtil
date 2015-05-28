c6ffa2bdc05af50490c263e8c13e88321e626ce0
ragnraok <okone1288@gmail.com>
2015-05-29 00:23:38 +0800

    add MethodInfo class

---
+++ src/main/java/com/ragnarok/jparseutil/dataobject/MethodInfo.java
@@ -0,0 +1,38 @@
+package com.ragnarok.jparseutil.dataobject;
+
+import java.util.ArrayList;
+
+/**
+ * Created by ragnarok on 15/5/29.
+ * represent information about a method
+ */
+public class MethodInfo {
+    
+    private String methodName;
+    private String returnType; // fully qualify name
+    private ArrayList<String> methodParamsType = new ArrayList<>(); // the parameters' type(fully qualify), in the order of declare in method
+    
+    public void setMethodName(String name) {
+        this.methodName = name;
+    }
+    
+    public String getMethodName() {
+        return this.methodName;
+    }
+    
+    public void setReturnType(String type) {
+        this.returnType = type;
+    } 
+    
+    public String getReturnType() {
+        return this.returnType;
+    }
+    
+    public void addParamType(String type) {
+        this.methodParamsType.add(type);
+    }
+    
+    public ArrayList<String> getParamType() {
+        return this.methodParamsType;
+    }
+}
