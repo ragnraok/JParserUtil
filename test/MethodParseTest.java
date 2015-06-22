66dcca848bac142269c3e6e716afbc7774996eb0
ragnraok <okone1288@gmail.com>
2015-06-23 00:38:51 +0800

    add method parse testcase

---
+++ test/MethodParseTest.java
@@ -0,0 +1,17 @@
+package com.rangarok.jparserutil.test;
+
+import java.util.Date;
+
+public class Foo {
+    public void foo() {
+        
+    }
+    
+    public void fooWithParam(int a, String b) {
+        
+    }
+    
+    public String fooWithParamReturnType(int a, String b, Date c) {
+        
+    }
+}
