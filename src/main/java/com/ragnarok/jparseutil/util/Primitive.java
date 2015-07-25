edd12a476819eedc662ec254f65df197c78e1c77
ragnraok <okone1288@gmail.com>
2015-07-25 17:48:56 +0800

    little fixed

---
+++ src/main/java/com/ragnarok/jparseutil/util/Primitive.java
@@ -0,0 +1,25 @@
+package com.ragnarok.jparseutil.util;
+
+/**
+ * Created by ragnarok on 15/7/25.
+ */
+public class Primitive {
+    public static String INT_TYPE = "int";
+    public static String INTEGER_TYPE = "Integer";
+    public static String STRING_TYPE = "String";
+    public static String FLOAT_TYPE = "float";
+    public static String FLOAT_PKG_TYPE = "Float";
+    public static String DOUBLE_TYPE = "double";
+    public static String DOUBLE_PKG_TYPE = "Double";
+    public static String CHAR_TYPE = "char";
+    public static String CHARACTER_TYPE = "Character";
+    public static String LONG_TYPE = "long";
+    public static String LONG_PKG_TYPE = "Long";
+    public static String BOOLEAN_TYPE = "boolean";
+    public static String BOOLEAN_PKG_TYPE = "Boolean";
+    public static String NUMBER_TYPE = "Number";
+
+    public static String[] PrimitiveTypes = new String[] {INT_TYPE, INTEGER_TYPE, STRING_TYPE,
+            FLOAT_TYPE, FLOAT_PKG_TYPE, DOUBLE_TYPE, DOUBLE_PKG_TYPE, CHAR_TYPE, CHARACTER_TYPE,
+            LONG_TYPE, LONG_PKG_TYPE, BOOLEAN_TYPE, BOOLEAN_PKG_TYPE, NUMBER_TYPE};
+}
--- src/main/java/com/ragnarok/jparseutil/util/Util.java
+++ src/main/java/com/ragnarok/jparseutil/util/Util.java
@@ -10,35 +10,13 @@ import com.sun.tools.javac.tree.JCTree;
  */
 public class Util {
     
-    public static String INT_TYPE = "int";
-    public static String INTEGER_TYPE = "Integer";
-    public static String STRING_TYPE = "String";
-    public static String FLOAT_TYPE = "float";
-    public static String FLOAT_PKG_TYPE = "Float";
-    public static String DOUBLE_TYPE = "double";
-    public static String DOUBLE_PKG_TYPE = "Double";
-    public static String CHAR_TYPE = "char";
-    public static String CHARACTER_TYPE = "Character";
-    public static String LONG_TYPE = "long";
-    public static String LONG_PKG_TYPE = "Long";
-    public static String BOOLEAN_TYPE = "boolean";
-    public static String BOOLEAN_PKG_TYPE = "Boolean";
-    public static String NUMBER_TYPE = "Number";
-    
-    private static String[] PrimitiveType = new String[] {INT_TYPE, INTEGER_TYPE, STRING_TYPE,
-            FLOAT_TYPE, FLOAT_PKG_TYPE, DOUBLE_TYPE, DOUBLE_PKG_TYPE, CHAR_TYPE, CHARACTER_TYPE,
-            LONG_TYPE, LONG_PKG_TYPE, BOOLEAN_TYPE, BOOLEAN_PKG_TYPE, NUMBER_TYPE};
-    
     public static final String JAVA_FILE_SUFFIX = ".java";
-    
-    private static String SYSTEM_DEFAULT_PACKAGE = "java.lang.";
-    public static String STRING_NULL_LITERAL = "null";
     
     public static boolean isPrimitive(String variableTypeName) {
         if (variableTypeName == null) {
             return false;
         }
-        for (String type : PrimitiveType) {
+        for (String type : Primitive.PrimitiveTypes) {
             if (type.equals(variableTypeName)) {
                 return true;
             }
