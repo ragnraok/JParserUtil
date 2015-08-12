0d31b7c85b8036cca19e5af0ff68c51596bf9474
ragnraok <okone1288@gmail.com>
2015-08-13 01:20:44 +0800

    remove unused testcase

--- test/AnnotationTest.java
+++
@@ -1,7 +0,0 @@
-package com.rangarok.jparserutil.test.annotation;
-
-public @interface Annotation {
-//    String[] value() default {"123", "123123"};
-//    String foo();
-    String bar() default "123123";
-}
--- test/InterfaceParseTest.java
+++
@@ -1,8 +0,0 @@
-package com.rangarok.jparserutil.test.annotation;
-
-public interface FooBar {
-    public static final String TAG = "test";
-    
-    void testMethod();
-    int testMethod2(int c, String b);
-}
